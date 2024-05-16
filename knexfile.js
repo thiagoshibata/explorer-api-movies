0413583468d6bf53881d91a2574df555fe1e34de
thiago-shibata <thiago.shibatta@gmail.com>
2024-05-16 12:10:09 -0300

    feat: config migrations in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,14 @@ module.exports = {
     pool: {
       afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
     },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
   },
 }
