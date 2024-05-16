ebe7da538d9c35acbab2830a412f946aed933e80
thiago-shibata <thiago.shibatta@gmail.com>
2024-05-16 11:16:30 -0300

    feat: config routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,9 @@
-estrucuted 
+const  { Router } = require("express")
 
+const routes = Router()
+
+routes.get("/", (request, response) => {
+  response.send("First router")
+})
+
+module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 require("express-async-errors")
 const AppError = require("./utils/AppError")
 const express = require("express")
+const routes = require("./routes")
 
 const app = express()
 app.use(express.json())
+app.use(routes)
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
