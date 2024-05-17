43132c0a19a2e4ce7b2f504716cc2310c3624fa1
thiago-shibata <thiago.shibatta@gmail.com>
2024-05-17 14:42:40 -0300

    feat: config routes movies

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const { Router } = require("express")
 const routes = Router()
 
 const usersRouter = require("./user.routes")
+const moviesRouter = require("./movies.routes")
 
 routes.use("/users", usersRouter)
+routes.use("/movies", moviesRouter)
 
 module.exports = routes
