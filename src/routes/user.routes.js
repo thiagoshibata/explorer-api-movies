dba6d7ec20fc95cf29428811a7594b81ee325946
thiago-shibata <thiago.shibatta@gmail.com>
2024-06-07 22:07:48 -0300

    feat: finalized upload img profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,28 @@
 const { Router } = require("express")
+const multer = require("multer")
+
+const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/UsersController")
+const UserAvatarController = require("../controllers/UserAvatarController")
 const ensureAuthenticated = require("../Middleware/ensureAuthenticated")
 
-const userRouter = Router()
+const userRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
-userRouter.post("/", usersController.create)
-userRouter.get("/:id", usersController.show)
-userRouter.get("/", usersController.index)
-userRouter.put("/", ensureAuthenticated, usersController.update)
-userRouter.delete("/:id", usersController.delete)
+userRoutes.post("/", usersController.create)
+userRoutes.get("/:id", usersController.show)
+userRoutes.get("/", usersController.index)
+userRoutes.put("/", ensureAuthenticated, usersController.update)
+userRoutes.delete("/:id", usersController.delete)
+userRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+)
 
-module.exports = userRouter
+module.exports = userRoutes
