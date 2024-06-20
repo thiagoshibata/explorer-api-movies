99568f6fd25b5211ad4a58669b375e84a5c9300b
thiago-shibata <thiago.shibatta@gmail.com>
2024-06-20 10:59:37 -0300

    feat: add ensureAuthenticated on routes

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,12 +1,17 @@
 const { Router } = require("express")
 const MoviesController = require("../controllers/MoviesController")
 
+const ensureAuthenticated = require("../Middleware/ensureAuthenticated")
+
 const movieRouter = Router()
 
 const moviesController = new MoviesController()
 
-movieRouter.post("/:user_id", moviesController.create)
+movieRouter.use(ensureAuthenticated)
+
+movieRouter.post("/", moviesController.create)
 movieRouter.get("/:id", moviesController.show)
 movieRouter.get("/", moviesController.index)
+movieRouter.delete("/", moviesController.delete)
 
 module.exports = movieRouter
